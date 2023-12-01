e1768f7facf2a378f4a038ae6f865450f0426fd2
meen0 <138549015+meen0@users.noreply.github.com>
2023-12-01 11:56:16 +0900

    Revert "feature/2-attend"

--- thesilverback/src/main/java/com/kimleepark/thesilver/attend/domain/repository/AttendRepository.java
+++ thesilverback/src/main/java/com/kimleepark/thesilver/attend/domain/repository/AttendRepository.java
@@ -1,18 +1,19 @@
 package com.kimleepark.thesilver.attend.domain.repository;
 
 import com.kimleepark.thesilver.attend.domain.Attend;
-import com.kimleepark.thesilver.employee.Employee;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface AttendRepository extends JpaRepository<Attend,Integer> {
 
-    List<Attend> findByEmployeeCodeAndAttendDateBetween(Employee employeeCode, LocalDate start, LocalDate end);
+    List<Attend> findByEmployeeCodeAndAttendDateBetween(int employeeCode, LocalDate start, LocalDate end);
 
-    Optional<Attend> findByEmployeeCodeAndAttendDate(Employee empNo, LocalDate date);
-
-    boolean existsByEmployeeCodeAndAttendDate(Employee empNo, LocalDate today);
+    Optional<Attend> findByEmployeeCodeAndAttendDate(int empNo, LocalDate date);
 }
